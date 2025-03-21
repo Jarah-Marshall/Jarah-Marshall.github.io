0499a54545b628a142a9420fadd173b47e2e5270
Jarah <jama8738@colorado.edu>
2025-03-21 09:43:12 -0600

    Changed the story a bit more

--- wa/wa10/main.js
+++ wa/wa10/main.js
@@ -4,7 +4,7 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a swim in Antartica. When they got to :inserty:, they stared in horror for a few moments, then out of nowhere they :insertz:. Bob saw the whole thing, they were confused with how it was possible. But was not surprised — :insertx: weighs 300 pounds, and it was a hot day. What else could have happened when you swim in Antartica on a hot day?';
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went to go make a sandwich. When they got to :inserty:, they stared in horror for a few moments. There was no ingredients to make their sandwich. Then out of nowhere they :insertz:. Bob saw the whole thing, they were confused with how it was possible. But was not surprised — :insertx: weighs 300 pounds, and it was a hot day. Little did they know that Bob was the one with the ingredients to the sandwich the entire time';
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 const insertY = ['the soup kitchen','Disneyland','the White House'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
